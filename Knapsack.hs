13e57a1f6b2dba0468b6e6dc1dbf9f4043b1e5f6
Unexp1ainable <samorepka314@gmail.com>
2023-02-28 22:15:06 +0100

    relocate type definitions

---
+++ Knapsack.hs
@@ -0,0 +1,39 @@
+module Knapsack where
+
+-- Item of Knapsack
+data Item = Item
+  { weight :: Int,
+    cost :: Int
+  }
+
+instance Show Item where
+  show :: Item -> String
+  show (Item weight cost) = "Item {\nweight: " ++ show weight ++ "\ncost: " ++ show cost ++ "\n}\n"
+
+-- Knapsack type
+data Knapsack = Knapsack
+  { maxWeight :: Int,
+    minCost :: Int,
+    items :: [Item]
+  }
+
+-- helper functions for formatting output
+removeLast :: [a] -> [a]
+removeLast [] = []
+removeLast [_] = []
+removeLast (x : xs) = x : removeLast xs
+
+-- change format of default Show string
+-- remove last \t and prepend additional \t
+formatItemListStr :: String -> String
+formatItemListStr = removeLast . (++) "\t" . foldr format ""
+  where
+    format :: Char -> String -> String
+    format x acc
+      | x == '[' || x == ']' || x == ',' = acc
+      | x == '\n' = "\n\t" ++ acc
+      | otherwise = x : acc
+
+instance Show Knapsack where
+  show :: Knapsack -> String
+  show (Knapsack maxWeight minCost items) = "Knapsack {\nmaxWeight: " ++ show maxWeight ++ "\nminCost: " ++ show minCost ++ "\nitems: [\n" ++ formatItemListStr (show items) ++ "]\n}\n"
--- xrepka07.hs
+++ xrepka07.hs
@@ -1,45 +1,8 @@
 module Main where
 
+import Knapsack
 import System.Environment (getArgs)
 import System.Exit (die)
-
--- Item of Knapsack
-data Item = Item
-  { weight :: Int,
-    cost :: Int
-  }
-
-instance Show Item where
-  show :: Item -> String
-  show (Item weight cost) = "Item {\nweight: " ++ show weight ++ "\ncost: " ++ show cost ++ "\n}\n"
-
--- Knapsack type
-data Knapsack = Knapsack
-  { maxWeight :: Int,
-    minCost :: Int,
-    items :: [Item]
-  }
-
--- helper functions for formatting output
-removeLast :: [a] -> [a]
-removeLast [] = []
-removeLast [_] = []
-removeLast (x : xs) = x : removeLast xs
-
--- change format of default Show string
--- remove last \t and prepend additional \t
-formatItemListStr :: String -> String
-formatItemListStr = removeLast . (++) "\t" . foldr format ""
-  where
-    format :: Char -> String -> String
-    format x acc
-      | x == '[' || x == ']' || x == ',' = acc
-      | x == '\n' = "\n\t" ++ acc
-      | otherwise = x : acc
-
-instance Show Knapsack where
-  show :: Knapsack -> String
-  show (Knapsack maxWeight minCost items) = "Knapsack {\nmaxWeight: " ++ show maxWeight ++ "\nminCost: " ++ show minCost ++ "\nitems: [\n" ++ formatItemListStr (show items) ++ "]\n}\n"
 
 --------------------------------------
 data Mode = ECHO | BRUTEFORCE | OPTIMALIZE deriving (Enum, Show)
@@ -54,10 +17,6 @@ parseArgs [x]
   | otherwise = Nothing
 parseArgs _ = Nothing
 
--- Deserialize Item
-
--- Deserialize Knapsack
-
 ------------------------------------
 main :: IO ()
 main = do
@@ -66,3 +25,4 @@ main = do
   maybe (die "Invalid arguments.") print mode
 
   putStrLn "Done"
+  print (Item 10 10)
